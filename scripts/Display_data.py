7c5c7f76d247dc5de6fe361fd25bb58485d0b4f6
BirhanuGebisa <birhanugebisa>
2022-08-24 03:28:40 +0300

    update display info

--- scripts/Display_data.py
+++ scripts/Display_data.py
@@ -27,3 +27,8 @@ class DataFrame:
 
     def skewness(self)-> pd.DataFrame:
         print(self.df.skew())
+        
+    def find_agg(df: pd.DataFrame, agg_column: str, agg_metric: str, col_name: str, order=False) -> pd.DataFrame:
+    new_df = df.groupby(agg_column)[agg_column].agg(agg_metric).reset_index(name=col_name).sort_values(by=col_name,
+        ascending=order)
+    return new_df.reset_index(drop=True)
