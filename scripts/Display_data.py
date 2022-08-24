8945052d3318b0c916a270df66f6e451ef3ef25c
BirhanuGebisa <birhanugebisa>
2022-08-24 11:01:58 +0300

    update file scripts

--- scripts/Display_data.py
+++ scripts/Display_data.py
@@ -18,17 +18,15 @@ class DataFrame:
 
     def show_data_description(self) -> pd.DataFrame:
         return self.df.describe()
-
+    #show the correlations
     def show_correlation(self) -> pd.DataFrame:
         return self.df.corr()
-    
+    #count null values
     def get_null_counts(self)-> pd.DataFrame:
         print(self.df.isnull().sum())
-
+    #skwiness 
     def skewness(self)-> pd.DataFrame:
         print(self.df.skew())
         
-    def find_agg(df: pd.DataFrame, agg_column: str, agg_metric: str, col_name: str, order=False) -> pd.DataFrame:
-    new_df = df.groupby(agg_column)[agg_column].agg(agg_metric).reset_index(name=col_name).sort_values(by=col_name,
-        ascending=order)
-    return new_df.reset_index(drop=True)
+  
+        
