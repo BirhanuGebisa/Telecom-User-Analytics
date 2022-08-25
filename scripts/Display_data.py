599754f7954755fd4143fd840a933a79e23f9bda
BirhanuGebisa <birhanugebisa>
2022-08-26 00:20:13 +0300

    update display data

--- scripts/Display_data.py
+++ scripts/Display_data.py
@@ -8,6 +8,23 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import MinMaxScaler
 
 class DataFrame:
+    @staticmethod
+    def get_top_ten(df: pd.DataFrame, column: str, drop_index: bool = True) -> pd.DataFrame:
+        df.sort_values(column, ascending=False, inplace=True)
+        if drop_index:
+            df.reset_index(drop=True, inplace=True)
+            
+    def replace_outliers_with_iqr(df, columns):
+        for col in columns:
+            Q1, Q3 = df[col].quantile(0.25), df[col].quantile(0.75)
+            IQR = Q3 - Q1
+            cut_off = IQR * 1.5
+            lower, upper = Q1 - cut_off, Q3 + cut_off
+            df[col] = np.where(df[col] > upper, upper, df[col])
+            df[col] = np.where(df[col] < lower, lower, df[col])
+
+        return df.head(10)
+    
     def __init__(self, df: pd.DataFrame) -> None:
         self.df = df
 
@@ -43,6 +60,14 @@ class DataFrame:
             plt.figure(figsize=(8, 4)) 
             sns.scatterplot(data = df, x=cols[i][0], y=cols[i][1], s=20, color=colors[i])
             print(self.corrMatrix(df, cols[i]))
+            
+    def fix_outlier(df, column):
+        df[column] = np.where(df[column] > df[column].quantile(0.95), df[column].mode(),df[column])
+        
+        return df[column]
+
+    
+
         
   
         
