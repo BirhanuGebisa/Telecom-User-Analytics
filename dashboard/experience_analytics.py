c133bd220063cc5d611d04099be8d6359627a2ea
BirhanuGebisa <birhanugebisa>
2022-08-27 17:48:57 +0300

    experience

---
+++ dashboard/experience_analytics.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+import os
+import sys
+
+
+def app():
+
+    st.title("User Experience Analysis")
+
+    df_avgThr = pd.read_csv('data/top10avgThroughput.csv')
+    df_rtt = pd.read_csv('data/top10rtt.csv')
+    df_tcp = pd.read_csv('data/top10tcp.csv')
+    df_frqThr = pd.read_csv('data/most_freqAvgThr.csv')
+    df_frqrtt = pd.read_csv('data/most_freqRTT.csv')
+    df_frqtcp = pd.read_csv('data/most_freqTCP.csv')
+
+    st.header("Top 10 Users Experience analysis")
+    st.subheader("Average Throughput")
+    st.dataframe(df_avgThr)
+    st.bar_chart(df_avgThr['Average throughput'])
+
+    st.subheader("Round Trip Time")
+    st.dataframe(df_rtt)
+    st.bar_chart(df_rtt['Average RTT'])
+
+    st.subheader("TCP")
+    st.dataframe(df_tcp)
+    st.bar_chart(df_tcp['Average TCP'])
+
+    st.header("Most Frequenct Users")
+    st.subheader('Frquent Average Throughput')
+    st.dataframe(df_frqThr)
+    st.bar_chart(df_frqThr['0'])
+
+    st.subheader("Frquent Round Trip Time")
+    st.dataframe(df_frqrtt)
+    st.bar_chart(df_frqrtt['0'])
+
+    st.subheader("Frquent TCP")
+    st.dataframe(df_frqtcp)
+    st.bar_chart(df_frqtcp['0'])
+
+    st.header("Cluster with 3 group classification")
+    st.image('data/clusterExp.png')
