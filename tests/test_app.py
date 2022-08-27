c73468b7ae1b56ff65b4d0f4d6690662d7288fcd
BirhanuGebisa <birhanugebisa>
2022-08-27 17:47:05 +0300

    update testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,11 +13,6 @@ app = MultiApp()
 
 st.sidebar.markdown("""
 # TellCo's User Analytics
-### Multi-Page App
-This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
-### Modifications
-\t- Page Folder Based Access
-\t- Presentation changed to SideBar
 """)
 
 # Add all your application here
